41fe09146d12b03c1a9811dd14b726d5f4d9662e
Sultan <sitexpressorg@gmail.com>
2023-05-12 13:16:23 +0800

    env

--- index.js
+++ index.js
@@ -7,43 +7,9 @@ index.use(cors())
 index.use(express.json())
 
 
-// index.get('/', (req, res) => {
-//
-//     return axios.get(`https://api.flickr.com/services/feeds/photos_public.gne/?key=bdf7039443f9ef9296e976917b374b35&format=json&nojsoncallback=1`)
-//         .then((response) => {
-//             res.send(response.data)
-//             console.log('Express response message!')
-//
-//         })
-//         .catch(e => {
-//             res.send('Express error message!')
-//             console.log('Error:', e)
-//         })
-// })
-//
-// index.post('/photo', (req, res) => {
-//     console.log(req.body)
-//     return axios.get(`
-//     https://api.flickr.com/services/feeds/photos_public.gne/?key=bdf7039443f9ef9296e976917b374b35&format=json&tags=${req.body.data.params[0]}&tagmode=${req.body.data.params[1]}&nojsoncallback=1`
-//         )
-//         .then((response) => {
-//             res.send(response.data)
-//             console.log('Express response message!')
-//
-//         })
-//         .catch(e => {
-//             res.send('Express error message!')
-//             console.log('Error:', e)
-//         })
-// })
+index.get('/', (req, res) => {
 
-
-
-index.post('/', (req, res) => {
-    console.log(req.body)
-    return axios.get(`
-    https://api.flickr.com/services/feeds/photos_public.gne/?key=bdf7039443f9ef9296e976917b374b35&format=json&tags=${req.body.data.params[0]}&tagmode=${req.body.data.params[1]}&nojsoncallback=1`
-    )
+    return axios.get(`https://api.flickr.com/services/feeds/photos_public.gne/?key=bdf7039443f9ef9296e976917b374b35&format=json&nojsoncallback=1`)
         .then((response) => {
             res.send(response.data)
             console.log('Express response message!')
@@ -54,6 +20,26 @@ index.post('/', (req, res) => {
             console.log('Error:', e)
         })
 })
+
+index.post('/photo', (req, res) => {
+    console.log(req.body)
+    return axios.get(`
+    https://api.flickr.com/services/feeds/photos_public.gne/?key=bdf7039443f9ef9296e976917b374b35&format=json&tags=${req.body.data.params[0]}&tagmode=${req.body.data.params[1]}&nojsoncallback=1`
+        )
+        .then((response) => {
+            res.send(response.data)
+            console.log('Express response message!')
+
+        })
+        .catch(e => {
+            res.send('Express error message!')
+            console.log('Error:', e)
+        })
+})
+
+
+
+
 
 index.listen(process.env.PORT || 3333, (err) => {
     if(err) {
