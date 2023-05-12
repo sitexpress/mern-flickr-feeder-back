77892cffd630cd6c57e90095c030412bbfdd3146
Sultan <sitexpressorg@gmail.com>
2023-05-12 13:04:55 +0800

    env

--- index.js
+++ index.js
@@ -7,25 +7,43 @@ index.use(cors())
 index.use(express.json())
 
 
-index.get('/', (req, res) => {
+// index.get('/', (req, res) => {
+//
+//     return axios.get(`https://api.flickr.com/services/feeds/photos_public.gne/?key=bdf7039443f9ef9296e976917b374b35&format=json&nojsoncallback=1`)
+//         .then((response) => {
+//             res.send(response.data)
+//             console.log('Express response message!')
+//
+//         })
+//         .catch(e => {
+//             res.send('Express error message!')
+//             console.log('Error:', e)
+//         })
+// })
+//
+// index.post('/photo', (req, res) => {
+//     console.log(req.body)
+//     return axios.get(`
+//     https://api.flickr.com/services/feeds/photos_public.gne/?key=bdf7039443f9ef9296e976917b374b35&format=json&tags=${req.body.data.params[0]}&tagmode=${req.body.data.params[1]}&nojsoncallback=1`
+//         )
+//         .then((response) => {
+//             res.send(response.data)
+//             console.log('Express response message!')
+//
+//         })
+//         .catch(e => {
+//             res.send('Express error message!')
+//             console.log('Error:', e)
+//         })
+// })
 
-    return axios.get(`https://api.flickr.com/services/feeds/photos_public.gne/?key=bdf7039443f9ef9296e976917b374b35&format=json&nojsoncallback=1`)
-        .then((response) => {
-            res.send(response.data)
-            console.log('Express response message!')
 
-        })
-        .catch(e => {
-            res.send('Express error message!')
-            console.log('Error:', e)
-        })
-})
 
-index.post('/photo', (req, res) => {
+index.post('/', (req, res) => {
     console.log(req.body)
     return axios.get(`
     https://api.flickr.com/services/feeds/photos_public.gne/?key=bdf7039443f9ef9296e976917b374b35&format=json&tags=${req.body.data.params[0]}&tagmode=${req.body.data.params[1]}&nojsoncallback=1`
-        )
+    )
         .then((response) => {
             res.send(response.data)
             console.log('Express response message!')
